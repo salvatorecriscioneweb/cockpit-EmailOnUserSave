7c5ca258f6ae1034d445f8d13d8d4aff2d302811
Salvatore Criscione <hello@salvatorecriscione.com>
2021-02-23 12:28:06 +0100

    working emails

--- EmailOnUserSave/actions.php
+++ EmailOnUserSave/actions.php
@@ -3,62 +3,93 @@
 $app->on('cockpit.accounts.active', function ($name, $data) use ($app) {
   $settings = $app->storage->getKey('cockpit/options', 'emailonusersave.settings', []);
   
-  if ( $settings['sendOnActive']) {
-    // Send Active Email
+  if ( $settings['sendOnActive'] ) {
+    $email = isset($settings['emailActive']) ? $settings['emailActive'] : [];
+
+    if (!empty($email['to'])) {
+      $to = $email['to'];
+      $to = str_replace('[:user_mail]', $name['email'], $to);
+      $subject = !empty($email['subject']) ? $email['subject'] : 'New collection saved';
+      $body = isset($email['body']) ? $email['body'] : '';
+
+      $dataHtml = "<br><hr><br>\n";
+      foreach ($data as $key => $value) {
+        $dataHtml .= '<div class="field">';
+        $dataHtml .= "<div><b>${key}:</b></div>\n";
+        if ($key === '_modified' || $key === '_created') {
+          $value = date('Y-m-d H:i', $value);
+        }
+        if (is_string($value)) {
+          $dataHtml .= "<div>${value}</div>\n";
+        }
+        else {
+          $dataHtml .= "<pre>" . json_encode($value) . "</pre>\n";
+        }
+        $dataHtml .= "</div>\n";
+      }
+
+      $body = str_replace('[:data]', $dataHtml, $body);
+
+      $vars = [
+        'name' => $name,
+        'app' => $app,
+        'body' => $body,
+      ];
+
+      $message = $app->view('emailonusersave:emails/onsave.php', $vars);
+
+      $app->mailer->mail(
+        $email['to'],
+        $subject,
+        $message
+      );
+    }
   }
 });
 
 $app->on('cockpit.accounts.create', function ($name, $data) use ($app) {
   $settings = $app->storage->getKey('cockpit/options', 'emailonusersave.settings', []);
   
-  if ($settings['sendOnCreate']) {
-    // Send Create Email
-  }
-});
+  if ($settings['emailCreate'] ) {
+    $email = isset($settings['emailCreate']) ? $settings['emailCreate'] : [];
 
-/*
-$app->on('collections.save.after', function ($name, $data) use ($app) {
-  $settings = $app->storage->getKey('cockpit/options', 'emailonusersave.settings', []);
+    if (!empty($email['to'])) {
+      $to = $email['to']
+      $to = str_replace('[:user_mail]', $name['email'], $to);
+      $subject = !empty($email['subject']) ? $email['subject'] : 'New collection saved';
+      $body = isset($email['body']) ? $email['body'] : '';
 
-  $collections = isset($settings['collections']) ? $settings['collections'] : [];
-  $email = isset($settings['email']) ? $settings['email'] : [];
-
-  if (in_array($name, $collections) && !empty($email['to'])) {
-    $subject = !empty($email['subject']) ? $email['subject'] : 'New collection saved';
-    $body = isset($email['body']) ? $email['body'] : '';
-
-    $dataHtml = "<br><hr><br>\n";
-    foreach ($data as $key => $value) {
-      $dataHtml .= '<div class="field">';
-      $dataHtml .= "<div><b>${key}:</b></div>\n";
-      if ($key === '_modified' || $key === '_created') {
-        $value = date('Y-m-d H:i', $value);
+      $dataHtml = "<br><hr><br>\n";
+      foreach ($data as $key => $value) {
+        $dataHtml .= '<div class="field">';
+        $dataHtml .= "<div><b>${key}:</b></div>\n";
+        if ($key === '_modified' || $key === '_created') {
+          $value = date('Y-m-d H:i', $value);
+        }
+        if (is_string($value)) {
+          $dataHtml .= "<div>${value}</div>\n";
+        }
+        else {
+          $dataHtml .= "<pre>" . json_encode($value) . "</pre>\n";
+        }
+        $dataHtml .= "</div>\n";
       }
-      if (is_string($value)) {
-        $dataHtml .= "<div>${value}</div>\n";
-      }
-      else {
-        $dataHtml .= "<pre>" . json_encode($value) . "</pre>\n";
-      }
-      $dataHtml .= "</div>\n";
+
+      $body = str_replace('[:data]', $dataHtml, $body);
+
+      $vars = [
+        'name' => $name,
+        'app' => $app,
+        'body' => $body,
+      ];
+
+      $message = $app->view('emailonusersave:emails/onsave.php', $vars);
+
+      $app->mailer->mail(
+        $email['to'],
+        $subject,
+        $message
+      );
     }
-
-    $body = str_replace('[:data]', $dataHtml, $body);
-
-    $vars = [
-      'name' => $name,
-      'app' => $app,
-      'body' => $body,
-    ];
-
-    $message = $app->view('emailonusersave:emails/onsave.php', $vars);
-
-    $app->mailer->mail(
-      $email['to'],
-      $subject,
-      $message
-    );
-
   }
 });
-*/
